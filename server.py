fa3f57d2b9d81c22968cf96a50e92cd191769f44
Wilco de Boer <deboer.wilco@gmail.com>
2022-03-28 14:30:49 +0200

    Use lowercase for routes and redirect uppercase.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from ruamel.yaml import YAML
 from pathlib import Path
 import re
@@ -13,13 +13,20 @@ for protocol in ['imap', 'smtp']:
     }
 
 
+@app.errorhandler(404)
+def redirect_to_lower(error):
+    if request.url.islower():
+        return error, 404
+    return redirect(request.url.lower(), 308)
+
+
 @app.route('/mail/config-v1.1.xml', methods=['GET'])
 def thunderbird():
     email = request.args.get('emailaddress')
     return render_template('thunderbird.xml', email=email, **config)
 
 
-@app.route('/Autodiscover/Autodiscover.xml', methods=['POST'])
+@app.route('/autodiscover/autodiscover.xml', methods=['POST'])
 def outlook():
     email = re.search(r'<EMailAddress>(.+)</EMailAddress>', request.data.decode('UTF8'))
     if email is None:
